643d632245e394fd74dedcc95e85edc1e076658a
Muhammad Rizky <44420394+arkariz@users.noreply.github.com>
2026-04-21 11:15:18 +0700

    feat(dependencies): add equatable

--- shared/dependencies/lib/dependencies.dart
+++ shared/dependencies/lib/dependencies.dart
@@ -1,3 +1,4 @@
 export 'package:dio/dio.dart' hide Headers;
+export 'package:equatable/equatable.dart';
 export 'package:json_annotation/json_annotation.dart';
 export 'package:retrofit/retrofit.dart';
