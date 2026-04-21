5d05241bb296fd5cf0cbb3421f647124c7104ea6
Muhammad Rizky <muhammad.rizky@bankcapital.co.id>
2026-04-21 12:01:49 +0700

    feat(failures)!: remove RestApiHandler and related documentation

--- core/failures/lib/failures.dart
+++ core/failures/lib/failures.dart
@@ -5,25 +5,14 @@
 /// - [FailureCode] for typed, extensible error identification
 /// - [FailureDetails] for infrastructure observability metadata
 /// - [RecoveryOptions] for client-side recovery behavior
-/// - [RestApiHandler] contract for transport-agnostic API execution
-///
-/// ## Quick Example
-///
-/// ```dart
-/// Future<T> runApi<T>(RestApiHandler rest, Future<T> Function() request) {
-///   return rest.handle(request);
-/// }
-/// ```
 library;
 
 import 'package:failures/src/failure.dart';
 import 'package:failures/src/failure_code.dart';
 import 'package:failures/src/failure_details.dart';
 import 'package:failures/src/recovery_options.dart';
-import 'package:failures/src/rest_api_handler.dart';
 
 export 'src/failure.dart';
 export 'src/failure_code.dart';
 export 'src/failure_details.dart';
 export 'src/recovery_options.dart';
-export 'src/rest_api_handler.dart';
