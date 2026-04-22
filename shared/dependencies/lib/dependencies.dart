5e816d493398e3e67d3dd1db0cfe13b3452e1112
Muhammad Rizky <muhammad.rizky@bankcapital.co.id>
2026-04-22 15:49:56 +0700

    feat(dependencies): add fpdart package to dependencies and exports

--- shared/dependencies/lib/dependencies.dart
+++ shared/dependencies/lib/dependencies.dart
@@ -1,4 +1,5 @@
 export 'package:dio/dio.dart' hide Headers;
 export 'package:equatable/equatable.dart';
+export 'package:fpdart/fpdart.dart';
 export 'package:json_annotation/json_annotation.dart';
 export 'package:retrofit/retrofit.dart';
